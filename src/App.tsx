89e6d04af1c513c4e237d24a216e3171cfefbece
ismaelp96 <ismael6991@live.com>
2025-05-13 14:45:52 -0300

    feat: Configurando Eslint e Prettier #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import { Button } from './components/ui/button';
 import './global.css';
 export function App() {
-	return <h1 className='bg-amber-500 text-amber-500'>Hello, world</h1>;
+	return (
+		<div className='flex h-screen items-center justify-center'>
+			<Button variant={'ghost'}>Enviar</Button>
+		</div>
+	);
 }

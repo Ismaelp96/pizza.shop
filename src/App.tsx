e3792a4a41d51dc91b515d53d8c12fc953950797
ismaelp96 <ismael6991@live.com>
2025-05-13 14:59:36 -0300

    feat: Instalando React Router #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Button } from './components/ui/button';
+import { RouterProvider } from 'react-router';
+
 import './global.css';
+
+import { router } from './routes';
+
 export function App() {
-	return (
-		<div className='flex h-screen items-center justify-center'>
-			<Button variant={'ghost'}>Enviar</Button>
-		</div>
-	);
+	return <RouterProvider router={router} />;
 }

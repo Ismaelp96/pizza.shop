a29811e29cffbdd545965a9fc708065db86f660e
ismaelp96 <ismael6991@live.com>
2025-05-15 14:46:11 -0300

    feat: Loading state no perfil

--- src/components/header/account-menu.tsx
+++ src/components/header/account-menu.tsx
@@ -13,33 +13,48 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
+import { Skeleton } from '../ui/skeleton';
 
 export function AccountMenu() {
-	const { data: profile } = useQuery({
+	const { data: profile, isLoading: isLoadingProfile } = useQuery({
 		queryKey: ['profile'],
 		queryFn: getProfile,
 	});
 
-	const { data: manageRestaurant } = useQuery({
-		queryKey: ['managed-restaurant'],
-		queryFn: getManagedRestaurant,
-	});
+	const { data: manageRestaurant, isLoading: isLoadingManagedRestaurant } =
+		useQuery({
+			queryKey: ['managed-restaurant'],
+			queryFn: getManagedRestaurant,
+		});
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
 				<Button
 					variant='outline'
 					className='flex items-center gap-2 select-none'>
-					{manageRestaurant?.name}
+					{isLoadingManagedRestaurant ? (
+						<Skeleton className='h-4 w-30' />
+					) : (
+						manageRestaurant?.name
+					)}
 					<ChevronDown className='h-4 w-4' />
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align='end' className='w-56'>
 				<DropdownMenuLabel className='flex flex-col'>
-					<span>{profile?.name}</span>
-					<span className='text-muted-foreground text-xs font-normal'>
-						{profile?.email}
-					</span>
+					{isLoadingProfile ? (
+						<div className='space-y-1.5'>
+							<Skeleton className='h-3 w-24' />
+							<Skeleton className='h-4 w-32' />
+						</div>
+					) : (
+						<>
+							<span>{profile?.name}</span>
+							<span className='text-muted-foreground text-xs font-normal'>
+								{profile?.email}
+							</span>
+						</>
+					)}
 				</DropdownMenuLabel>
 				<DropdownMenuSeparator />
 				<DropdownMenuItem>

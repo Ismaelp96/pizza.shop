0534c6a5ad96b032b34e685ed15b42d795854bd7
ismaelp96 <ismael6991@live.com>
2025-05-30 11:16:34 -0300

    feat: Receita no período #3

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,11 +1,8 @@
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from '@/components/ui/card';
-import { DollarSign } from 'lucide-react';
+import { useMemo, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import type { DateRange } from 'react-day-picker';
+import { subDays } from 'date-fns';
+import colors from 'tailwindcss/colors';
 import {
 	ResponsiveContainer,
 	LineChart,
@@ -14,40 +11,42 @@ import {
 	Line,
 	CartesianGrid,
 } from 'recharts';
-import colors from 'tailwindcss/colors';
 
-const data = [
-	{
-		date: '10/12',
-		revenue: 1200,
-	},
-	{
-		date: '11/12',
-		revenue: 3000,
-	},
-	{
-		date: '12/12',
-		revenue: 500,
-	},
-	{
-		date: '13/12',
-		revenue: 2500,
-	},
-	{
-		date: '14/12',
-		revenue: 800,
-	},
-	{
-		date: '15/12',
-		revenue: 900,
-	},
-	{
-		date: '16/12',
-		revenue: 2000,
-	},
-];
+import { getDailyRevenueInPeriod } from '@/api/get-daily-revenue-in-period';
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { DateRangerPicker } from '@/components/date-ranger-picker';
 
 export function RevenueChart() {
+	const [dateRange, setDateRange] = useState<DateRange | undefined>({
+		from: subDays(new Date(), 7),
+		to: new Date(),
+	});
+	const { data: dailyRevenueInPeriod } = useQuery({
+		queryKey: ['metrics', 'daily-revenue-in-period', dateRange],
+		queryFn: () =>
+			getDailyRevenueInPeriod({
+				from: dateRange?.from,
+				to: dateRange?.to,
+			}),
+	});
+
+	const chartData = useMemo(() => {
+		return dailyRevenueInPeriod?.map((chartItem) => {
+			return {
+				date: chartItem.date,
+				receipt: chartItem.receipt / 100,
+			};
+		});
+	}, [dailyRevenueInPeriod]);
+
 	return (
 		<Card className='col-span-6'>
 			<CardHeader className='flex flex-row items-center justify-between pb-8'>
@@ -58,40 +57,45 @@ export function RevenueChart() {
 						</CardTitle>
 						<CardDescription>Receita diária no período</CardDescription>
 					</div>
-					<DollarSign className='text-muted-foreground h-4 w-4' />
+				</div>
+				<div className='flex items-center gap-3'>
+					<Label>Período</Label>
+					<DateRangerPicker date={dateRange} onDateChange={setDateRange} />
 				</div>
 			</CardHeader>
 			<CardContent>
-				<ResponsiveContainer width='100%' height={240}>
-					<LineChart data={data} style={{ fontSize: 12 }}>
-						<XAxis
-							stroke='#888'
-							dataKey='date'
-							tickLine={false}
-							axisLine={false}
-							dy={16}
-						/>
-						<YAxis
-							stroke='#888'
-							axisLine={false}
-							tickLine={false}
-							width={80}
-							tickFormatter={(value: number) =>
-								value.toLocaleString('pt-BR', {
-									style: 'currency',
-									currency: 'BRL',
-								})
-							}
-						/>
-						<CartesianGrid vertical={false} className='stroke-muted' />
-						<Line
-							type='linear'
-							strokeWidth={2}
-							dataKey='revenue'
-							stroke={colors.violet[500]}
-						/>
-					</LineChart>
-				</ResponsiveContainer>
+				{chartData && (
+					<ResponsiveContainer width='100%' height={240}>
+						<LineChart data={chartData} style={{ fontSize: 12 }}>
+							<XAxis
+								stroke='#888'
+								dataKey='date'
+								tickLine={false}
+								axisLine={false}
+								dy={16}
+							/>
+							<YAxis
+								stroke='#888'
+								axisLine={false}
+								tickLine={false}
+								width={80}
+								tickFormatter={(value: number) =>
+									value.toLocaleString('pt-BR', {
+										style: 'currency',
+										currency: 'BRL',
+									})
+								}
+							/>
+							<CartesianGrid vertical={false} className='stroke-muted' />
+							<Line
+								type='linear'
+								strokeWidth={2}
+								dataKey='receipt'
+								stroke={colors.violet[500]}
+							/>
+						</LineChart>
+					</ResponsiveContainer>
+				)}
 			</CardContent>
 		</Card>
 	);
